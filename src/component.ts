73aabbc9d7479ed00963bc80ae3b1971b8fde331
Tushar Mathur <tusharmath@gmail.com>
2018-07-11 23:18:55 +0530

    refactor(component): remove snabbdom dependency

--- src/component.ts
+++ src/component.ts
@@ -1,9 +1,8 @@
 import {Action} from 'action-type'
 import {Hoe} from 'hoe'
-import {VNode} from 'snabbdom/vnode'
 
-export interface Component<State, Params> {
-  init: (p: Partial<State>) => State
+export type Component<State, Params, VNode> = {
+  init: {(p?: Partial<State>): State}
   update: <T>(action: Action<T>, state: State) => State
   command: <T, R>(action: Action<T>, state: State) => Action<R>
   view: (e: Hoe, m: State, p: Params) => VNode
